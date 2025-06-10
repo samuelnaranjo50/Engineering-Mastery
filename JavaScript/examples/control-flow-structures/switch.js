4a8a64856f36eb57e5edd71a32f3a88167035c5b
Samuel Naranjo <samuelnaranjo505@gmail.com>
2025-06-10 18:16:38 -0500

    **JavaScript Control flow - Switch**
    Identifying the slightly differences between switch statement in C and JavaScript

---
+++ JavaScript/examples/control-flow-structures/switch.js
@@ -0,0 +1,21 @@
+var grade = 6;
+
+switch (grade) {
+  case 1:
+    console.log("You are allowed to enter");
+    break;
+  case 2:
+    console.log("You are allowed to enter");
+    break;
+  case 3:
+    console.log("You are allowed to enter");
+    break;
+  case 4:
+    console.log("You are allowed to enter");
+    break;
+  case 5:
+    console.log("You are allowed to enter");
+    break;
+  default:
+    console.log("You are not allowed to enter X");
+}
