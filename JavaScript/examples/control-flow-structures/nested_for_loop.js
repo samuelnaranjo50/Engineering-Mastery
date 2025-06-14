22221155fefc45d06b9bfdceeb1fefb50c850c3e
Samuel Naranjo <samuelnaranjo505@gmail.com>
2025-06-13 20:07:11 -0500

    **JavaScript - Nested For Loops**
    Understanding and exploring in deep nested for loops possibilities using the next 2 examples: Multiplication table, Harvard - problem set (Mario).

---
+++ JavaScript/examples/control-flow-structures/nested_for_loop.js
@@ -0,0 +1,33 @@
+// Multiplication Table
+/*
+for (let i = 0; i <= 10; i++) {
+    console.log('Table of the:', i);
+    for (let j = 0; j <=10; j++){
+        console.log(i, '*', j, '=', i * j);
+    }
+}
+
+
+*/
+// Mario Pattern triangle
+
+var rows = 3; 
+
+for(let i = rows; i > 0; i--){
+    
+    for(let k = i; k > 1; k--){
+        process.stdout.write(' ');
+
+    }
+    for(let j = rows; j >= i; j--){
+        process.stdout.write('#');
+    }
+    process.stdout.write(' ');
+    for(let h = rows; h >= i; h--){
+        process.stdout.write('#');
+    }
+    console.log(' ');
+    
+    
+   
+}
