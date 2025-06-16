cf28a0bb0c3707531c1cccbe784ac0d5857167db
Samuel Naranjo <samuelnaranjo505@gmail.com>
2025-06-16 09:23:02 -0500

    Exploring the concepts related with functions, its parts and how parameters relate to arguments. Practical examples of how functions work in JS.

---
+++ JavaScript/examples/functions/multiplication_function.js
@@ -0,0 +1,11 @@
+
+
+function multiplication2N(number1, number2){
+    var result = number1 * number2;
+    console.log('The result of', number1, '*', number2, 'is:', result);
+}
+
+var num1 = 50;
+var num2 = 10;
+
+multiplication2N(num1, num2);
