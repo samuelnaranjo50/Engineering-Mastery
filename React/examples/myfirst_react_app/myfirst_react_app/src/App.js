ea89fe4efe2476f8c03cd6d3a4b9971169a1b418
Samuel Naranjo <samuelnaranjo505@gmail.com>
2025-08-06 06:51:44 +0200

    **My first modular React app  - Components, modularity & render syntax
    In this practical usage of basic react knowledge I used components, ES6 modules and JSX render syntax. To create a functional app that integrates properly the component tree to load all components, I render them In the app.js component.

---
+++ React/examples/myfirst_react_app/myfirst_react_app/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import Nav from './components/nav.js';
+import Promo from "./components/Promo.js";
+import Intro1 from './components/Intro1.js';
+import Intro2 from './components/Intro2.js';
+import Intro3 from './components/Intro3.js';
+import Footer from './components/Footer.js';
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Promo />
+      <Intro1 />
+      <Intro2 />
+      <Intro3 />
+      <Footer />
+
+    </>
+
+
+    
+
+  );
+}
+
+export default App;
