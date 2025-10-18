b3ffd7fb81c10acfe58ab34b7fb4167493a46291
Samuel Naranjo <samuelnaranjo505@gmail.com>
2025-10-18 07:07:21 -0500

    **Basic c++ program of addition**
    Using basic input & output c++ keywords to retrieve and display data from the console

---
+++ Algorithms/Exercises/sum_of_two_digits.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int main () {
+
+    int num1;
+    int num2;
+
+    std::cin >> num1;
+    std::cin >> num2;
+
+    int sum = num1 + num2;
+
+    std::cout << sum;
+
+    return 0;
+}
