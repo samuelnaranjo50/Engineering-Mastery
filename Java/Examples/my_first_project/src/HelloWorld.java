103fc1ea643a8c5122b3330fe42ff0a1ba714666
Samuel Naranjo <samuelnaranjo505@gmail.com>
2025-11-28 22:41:08 -0500

    **My first Java program**
    This java program uses a project structure to practice compiling classes in the proper folder and running them, this way I can maintain the files tidy.

---
+++ Java/Examples/my_first_project/src/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello world, this is my first program");
+        System.out.printf("This are some arguments %s & %s", args[0], args[1]);
+    }
+}
